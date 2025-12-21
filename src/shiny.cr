611d652813e7ef821eef45470e0f88f33fa03184
NitosMaster <nunoarrochella@gmail.com>
2025-12-21 18:53:13 +0000

    Added functional/barebone Panels and Titles

--- src/shiny.cr
+++ src/shiny.cr
@@ -2,24 +2,69 @@
 module Shiny
   VERSION = "0.1.0"
 
-  terminal-x, terminal-y = 'tput lines'.to_i, 'tput cols'.to_i
-
   module Renderable
 
   end
 
-  class panel
+  class Panel
+		@side : String
+  	@top : String
+  	@topLeft : String
+  	@topRight : String
+  	@bottomLeft : String
+  	@bottomRight : String
 
-    def initialize(@width = terminal-x, @height, @border = "round", @color = "white", @title = "")
+    def initialize(width : Int32? = nil, @height = 10, @border = "round", @color = "white", @title = "")
+        @width = width || `tput cols`.to_i
 
+        @side = "│"
+  			@top = "─"
+  			@topLeft = "╭"
+  			@topRight = "╮"
+  			@bottomLeft = "╰"
+ 	 			@bottomRight = "╯"
+
+        if @border == "round"
+          @side = "│"
+          @top = "─"
+          @topLeft = "╭"
+          @topRight = "╮"
+          @bottomLeft = "╰"
+          @bottomRight = "╯"
+        end
     end
 
     def render
 
     end
 
-    def render(content : Renderable)
+    def render(content : Renderable? = nil)
 
+      head = " #{@title} "
+ # thats ↑ what she said
+      remaining = @width - 2 - head.size
+      leftPad = remaining / 2
+      rightPad = remaining - leftPad
+
+      if content == nil
+        print @topLeft
+        if !@title.empty?
+          print @top * leftPad.to_i
+          print head
+          print @top * rightPad.to_i
+        else
+          print @top * (@width - 2)
+        end
+        puts @topRight
+        (@height - 2).times do
+          print @side
+          print " " * (@width -2)
+          puts @side
+        end
+        print @bottomLeft
+        print @top * (@width - 2)
+        puts @bottomRight
+      end
     end
   end
 end
