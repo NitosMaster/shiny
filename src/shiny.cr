e06a51837642cfa59c45a3e24e2047999c8d8d74
NitosMaster <nunoarrochella@gmail.com>
2025-12-21 23:16:00 +0000

    ANSI Colors

--- src/shiny.cr
+++ src/shiny.cr
@@ -7,14 +7,22 @@ module Shiny
   end
 
   class Panel
-		@side : String
-  	@top : String
-  	@topLeft : String
-  	@topRight : String
-  	@bottomLeft : String
-  	@bottomRight : String
+		def hex_to_rgb(hex : String)
+      hex = hex.lstrip('#')
 
-    def initialize(width : Int32? = nil, @height = 10, @border = "round", @color = "white", @title = "")
+      r = hex[0..1].to_i(16)  # First 2 chars
+      g = hex[2..3].to_i(16)  # Middle 2 chars
+      b = hex[4..5].to_i(16)  # Last 2 chars
+
+      {r, g, b}
+    end
+
+    def rgb_to_ansi(hex : String)
+      r, g, b = hex_to_rgb(hex)
+      "\e[38;2;#{r};#{g};#{b}m"
+    end
+
+    def initialize(width : Int32? = nil, @height = 10, @border = "round", @color = "#FFFFFF", @title = "")
         @width = width || `tput cols`.to_i
 
         @side = "│"
@@ -47,6 +55,7 @@ module Shiny
       rightPad = remaining - leftPad
 
       if content == nil
+        print rgb_to_ansi(@color)
         print @topLeft
         if !@title.empty?
           print @top * leftPad.to_i
@@ -64,6 +73,7 @@ module Shiny
         print @bottomLeft
         print @top * (@width - 2)
         puts @bottomRight
+        print "\e[0m"
       end
     end
   end
