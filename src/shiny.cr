40a6c8ef19824f00a542681a0bef3c50880db33d
NitosMaster <nunoarrochella@gmail.com>
2025-12-21 01:00:09 +0000

    Added basic defs and classes

--- src/shiny.cr
+++ src/shiny.cr
@@ -2,5 +2,24 @@
 module Shiny
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  terminal-x, terminal-y = 'tput lines'.to_i, 'tput cols'.to_i
+
+  module Renderable
+
+  end
+
+  class panel
+
+    def initialize(@width = terminal-x, @height, @border = "round", @color = "white", @title = "")
+
+    end
+
+    def render
+
+    end
+
+    def render(content : Renderable)
+
+    end
+  end
 end
