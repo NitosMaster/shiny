4b36e33f417e5cab16051ac288b863c66b75ec8c
NitosMaster <nunoarrochella@gmail.com>
2026-01-13 07:53:36 +0000

    fixed 3d.cr

--- 3d.cr
+++ 3d.cr
@@ -1,18 +1,24 @@
-require "./shiny/src/shiny.cr"
+require "./src/shiny.cr"
 
-main = Shiny::Panel.new(width*2, height, "round", "#B0
+width = 40
+height = 40
+
+panel = Shiny::Panel.new(width*2, height, "round", "#B0
 0B69", "Monitor")
 
-def renderer(x, y, z, char, height, width, focalLenght)
+def render(panel, x, y, z, char, height, width, focalLenght)
 	realx = (width // 2) + (x * focalLenght) // z
 	realy = (height // 2) + (y * focalLenght) // z
 
-	main.put(realx, realy, char)
-	main.renderBuffer()
+	panel.put(realx, realy, char)
+  panel.renderBuffer()
 end
 
-camHeight = 20
-camWidth = 20
+camHeight = 40
+camWidth = 40
 camZoom = 10
+x = 3
+y = 4
 z = 1
 
+render(panel, x, y, z, "n", camHeight, camWidth, camZoom)
